6856ba297c1df2178f70d6cac0db3431f2b59830
RomaMartynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2022-12-22 10:44:20 +0200

    Add project files.

---
+++ Movies Service/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Movies_Service.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
